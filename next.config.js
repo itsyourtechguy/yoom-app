cbb166dbdbbf7b9ac3acc6388e0d3b433b3c6d47
itsyourtechguy <crronaldo749@gmail.com>
2025-02-09 19:36:31 +0530

    updated

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/yoom-app',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
